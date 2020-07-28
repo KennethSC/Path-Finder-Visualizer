5251ea5c3b7c0837f01b8629fab13f9e91bb6226
KennethSC <kennethsantacruz12@gmail.com>
2020-07-27 21:56:06 -0700

    Mdoifying files

--- src/Visualizer/Visualizer.jsx
+++ src/Visualizer/Visualizer.jsx
@@ -2,7 +2,6 @@ import React, {Component} from 'react';
 import Node from './Nodes/Node';
 
 import './Visualizer.css';
-import {render} from '@testing-library/react';
 
 export default class Visualizer extends Component {
     constructor(props){
